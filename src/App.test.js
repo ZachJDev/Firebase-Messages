92ebb95913ed2498284ae88ece13322fe63f5fb5
Zach Johnson <Zachjdev2121@gmail.com>
2022-01-05 11:40:58 -0500

    modified tests to hopefully close open connections

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import {
+  getFirestore,
+  disableNetwork,
+  enableNetwork,
+} from "firebase/firestore";
+
+beforeAll(async () => {
+  await enableNetwork(getFirestore());
+});
+
+afterAll(async () => {
+  await disableNetwork(getFirestore());
+});
 
 test("renders learn react link", () => {
   render(<App />);

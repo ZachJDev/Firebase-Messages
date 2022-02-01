bd4b5b52d9fb585497890a12c4c534bc4dfa02bf
Zach Johnson <Zachjdev2121@gmail.com>
2022-02-01 14:21:45 -0500

    test scripts

--- functions/helloWorld/index.js
+++ functions/helloWorld/index.js
@@ -13,5 +13,5 @@ exports.helloWorld2 = functions.https.onRequest((request, response) => {
     "process.env??",
     process.env.BQ_ID
   );
-  response.send("Hello from Firebase!222332122");
+  response.send("Hello from Firebase!2223d32122");
 });

893ec36f93a22a225f6d577655c0c439cb94f8b4
Zach Johnson <Zachjdev2121@gmail.com>
2022-02-01 10:57:50 -0500

    test Function Deploys

---
+++ functions/hello/index.js
@@ -0,0 +1,17 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  functions.logger.info(
+    "Project:",
+    functions.config().env.project_id,
+    "dataset:",
+    functions.config().env.dataset,
+    "process.env??",
+    process.env.BQ_ID
+  );
+  response.send("Hello from Firebase!");
+});
